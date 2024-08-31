b10d09c30bcb1d9886ff4f2bf17c77d5d0a59ca5
Spencer Ewall <spencer.ewall@datadoghq.com>
2024-08-30 21:29:15 -0400

    changed package structure

--- src/gobyexample/hello.go
+++ src/gobyexample/hello.go
@@ -1,4 +1,4 @@
-package main
+package gobyexample
 
 // basic library for i/o and string formatting https://pkg.go.dev/fmt
 import "fmt"
@@ -8,17 +8,17 @@ func main() {
 	string2 := "or you can declare a variable and instatiate it with :="
 	fmt.Println(string1)
 	fmt.Println(string2)
-	
+
 	fmt.Println()
 
-	whileCounter := 0;
+	whileCounter := 0
 	for whileCounter < 5 {
 		whileCounter++
 		whileMsg := "go has no while loops, it's all f"
 		oCounter := 0
 		for oCounter < whileCounter {
 			whileMsg += "o"
-			oCounter++;
+			oCounter++
 		}
 		whileMsg += "r"
 		fmt.Println(whileMsg)
@@ -27,28 +27,28 @@ func main() {
 	// string2 := "but this will error"
 	fmt.Println()
 	for j := 0; j < 3; j++ {
-        fmt.Println("it's also got normal style for loops, but there are no parens")
-    }
+		fmt.Println("it's also got normal style for loops, but there are no parens")
+	}
 
 	fmt.Println()
 
 	for rangeCounter := range 6 {
 		fmt.Print(rangeCounter)
-        fmt.Println("it's also got syntactic sugar for ranges, just like ruby!")
-    }
+		fmt.Println("it's also got syntactic sugar for ranges, just like ruby!")
+	}
 
 	fmt.Println()
 
 	fmt.Println("For conditional control, go has if/else, and case/switch statements. BUT no ternerary operators!!")
 
 	if num := 9; num < 0 {
-        fmt.Println(num, "if else statements function basically as normal, except you can have multiple statements in a single line, which is pretty cool")
-    } else if num < 10 {
+		fmt.Println(num, "if else statements function basically as normal, except you can have multiple statements in a single line, which is pretty cool")
+	} else if num < 10 {
 		fmt.Println(num, "if else statements function basically as normal, except you can have multiple statements in a single line, which is pretty cool.")
 		fmt.Println("variables declared in prior conditional statements are available in later conditionals, which I can't imagine a greate use for yet")
-    } else {
-        fmt.Println("not here tho")
-    }
+	} else {
+		fmt.Println("not here tho")
+	}
 
 	fmt.Println()
 	fmt.Println("Switch statements are pretty powerful in go")
